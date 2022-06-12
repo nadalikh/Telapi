a1e46b861361a850300003ff7a655b119a710a12
nadali <nadalikh@gmail.com>
2022-06-12 13:44:53 +0200

    first commit

---
+++ hook.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+$bot_api_key  = '5362421313:AAGQm8uLoOFlVHk56ZVVAJJmTBKGIJfqYq0';
+$bot_username = 'nkhTestPhp_bot';
+    
+    // Create Telegram API object
+    $telegram = new Telegram($bot_api_key);
+	$chat = $telegram->ChatID();
+	$telegram->sendMessage(['chat_id'=>$chat, 'text'=>'test']);
+    // Handle telegram webhook request
+    //  Request::sendMessage(['text'=>"hei"]);
+    // Silence is golden!
+    // log telegram errors
+
