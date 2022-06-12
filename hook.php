3ac924efd0fcd97e8a24aa5d43c6d5d7cc68c62d
Nadali Khalili <nadalikh@gmai.com>
2022-06-12 17:36:54 +0430

    test

--- hook.php
+++ hook.php
@@ -7,9 +7,9 @@ $bot_username = 'nkhTestPhp_bot';
     // Create Telegram API object
     $telegram = new Telegram($bot_api_key);
 	$chat_id = $telegram->ChatID();
-    $chat = $telegram->getChat($chat_id);
+    $chat = json_decode($telegram->getChat($chat_id));
 //    $retured = exec("echo 'ali' >> /var/www/html/nkhpro/public/telegram/logs");
-	$telegram->sendMessage(['chat_id'=>$chat_id, 'text'=> $chat->username]);
+	$telegram->sendMessage(['chat_id'=>$chat_id, 'text'=> $chat_id]);
     // Handle telegram webhook request
     //  Request::sendMessage(['text'=>"hei"]);
     // Silence is golden!
