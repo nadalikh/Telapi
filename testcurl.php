a2d56508d2ed5ba113161786876c224d47aec096
Nadali Khalili <nadalikh@gmai.com>
2022-06-24 15:03:23 +0430

    test

--- testcurl.php
+++ testcurl.php
@@ -5,5 +5,6 @@ curl_setopt_array(
     CURLOPT_URL => 'https://api.telegram.org/bot5362421313:AAGQm8uLoOFlVHk56ZVVAJJmTBKGIJfqYq0/sendMessage?chat_id='.$_GET['chat_id'].'&text='.$_GET['message'],
     CURLOPT_RETURNTRANSFER => true
 ));
+var_dump($_GET['message']);
 $output = curl_exec($ch);
 echo $output;
