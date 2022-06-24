0b10fc572dadf7b8cbacd04606c625cd32901bfe
Nadali Khalili <nadalikh@gmai.com>
2022-06-24 15:21:55 +0430

    test

--- testcurl.php
+++ testcurl.php
@@ -1,10 +1,16 @@
 <?php
 $ch = curl_init();
+$messages = explode(",",$_GET['message']);
+
+$m = "";
+foreach ($messages as $message)
+    $m .= $message."%0A";
+
 curl_setopt_array(
     $ch, array(
-    CURLOPT_URL => 'https://api.telegram.org/bot5362421313:AAGQm8uLoOFlVHk56ZVVAJJmTBKGIJfqYq0/sendMessage?chat_id='.$_GET['chat_id'].'&text='.$_GET['message']."%0Afffff",
+    CURLOPT_URL => 'https://api.telegram.org/bot5362421313:AAGQm8uLoOFlVHk56ZVVAJJmTBKGIJfqYq0/sendMessage?chat_id='.$_GET['chat_id'].'&text='.$m,
     CURLOPT_RETURNTRANSFER => true
 ));
-var_dump($_GET['message']);
+var_dump($messages);
 $output = curl_exec($ch);
 echo $output;
