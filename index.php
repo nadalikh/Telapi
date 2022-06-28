544f7e02b9ff0645971235fa5546ace1a88fb82c
Nadali Khalili <nadalikh@gmai.com>
2022-06-28 17:20:25 +0430

    test

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
                 <a href="#mobile" class="text-neutral-50 ms-nav-item block m-3 px-5 py-3 rounded-md transition-all">Mobile</a>
             </div>
         </div>
-        <div class="absolute left-1/2 top-1/2 -translate-x-1/2  -translate-y-1/2 grid grid-cols-2 grid-rows-1 justify-center m-1/2">
+        <div class="absolute left-1/2 top-1/2 -translate-x-1/2  -translate-y-1/2 grid grid-cols-2 grid-rows-1 justify-center w-1/2">
             <form method="post" class="m-auto">
                 <div class="ms-wrapper">
                     <label class="text-neutral-50 block" for="userid">Telegram Id (as your username)</label>
