3f7b10179b1abc4d9ba0bac93dcd5c6309121232
Nadali Khalili <nadalikh@gmai.com>
2022-06-28 12:42:44 +0430

    test

--- index.php
+++ index.php
@@ -8,11 +8,12 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/daisyui@2.17.0/dist/full.css" rel="stylesheet" type="text/css" />
+    <link href="./css/style.css" rel="stylesheet" type="text/css" />
     <script src="https://cdn.tailwindcss.com"></script>
     <title>NKH call center</title>
 </head>
 <body>
-    <div class="border-2 w-full h-full ">
+    <div class="ms-full-height border-2 w-full">
         <div class="navbar bg-base-100">
             <div class="flex-1">
                 <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
