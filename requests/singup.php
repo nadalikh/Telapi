3712844a4c2a7e3ba5f0b5af0b9e04bf844b2fda
Nadali Khalili <nadalikh@gmai.com>
2022-07-01 11:15:26 +0430

    test

--- requests/singup.php
+++ requests/singup.php
@@ -5,7 +5,7 @@ if(isset($_POST['signup'])){
     $truth &= preg_match("/^[a-zA-Z0-9@#$%&*_-]{5,}$/", $_POST['p1']);
     $truth &= preg_match("/^[a-zA-Z0-9@#$%&*_-]{5,}$/", $_POST['p2']);
     echo $truth;
-    $truth &= preg_match("/^[a-zA-Z]{,25}$/", $_POST['name']);
+    $truth &= preg_match("/^[a-zA-Z]{1,25}$/", $_POST['name']);
     $truth &= preg_match("/^[0-9]{3}$/", $_POST['extension']);
     $username = $_POST['userid'];
     $p1 = $_POST['p1'];
@@ -16,7 +16,6 @@ if(isset($_POST['signup'])){
     $extension = $_POST['extension'];
     if(!$truth)
         return json_encode(['message'=>'formats is not valid']);
-    echo "dfdf";
 
     if($p1 === $p2)
         return json_encode(['message'=>'password not confirmed']);
@@ -25,4 +24,6 @@ if(isset($_POST['signup'])){
     $db->addUser($username, $name, $role, $p);
     $db->assignExtensionTouser($extension, $name);
     return json_encode(['message'=>'You registered successfully']);
+    echo "dfdf";
+
 }
